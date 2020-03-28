99010f49c0bb10a8e17fdcdce87e6cf8a64a60ab
Burak Gokoglu <burak.gokoglu@gmail.com>
2020-03-27 19:20:21 -0500

    initial commit

---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using SimpleApi.Controllers;
+using Xunit;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ReturnFiveResults()
+        {
+            var weatherController = new WeatherForecastController();
+
+            var val = weatherController.Get();
+            Assert.Equal(5, val.Count());
+        }
+    }
+}
